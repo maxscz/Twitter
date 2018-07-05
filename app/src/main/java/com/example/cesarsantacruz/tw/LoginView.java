7f30bad234f1828e90b649b00f5b9a4680098ccd
LeoncioChiunti <chiunty09@gmail.com>
2018-07-05 16:58:30 -0500

    previo merge

---
+++ app/src/main/java/com/example/cesarsantacruz/tw/LoginView.java
@@ -0,0 +1,108 @@
+package com.example.cesarsantacruz.tw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//======================================================================================================================
+public class LoginView extends AppCompatActivity implements TextWatcher, View.OnClickListener {
+    Toolbar toolbar;
+    Button btLogingButton;
+    EditText etUserLoging;
+    EditText etPasswordLoging;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view);
+                                                            //Asignacion de xml toolbar a variable toolbar
+        Toolbar myToolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(myToolbar);
+                                                            //Asignacion de xml button a variable button
+        btLogingButton = findViewById(R.id.activity_loging_view_btLogingButton);
+
+        etUserLoging = findViewById(R.id.activity_loging_etUserLoging);
+
+        etPasswordLoging = findViewById(R.id.activity_loging_etPasswordLoging);
+
+
+        etUserLoging.addTextChangedListener(this);
+//----------------------------------------------------------------------------------------------------------------------
+        etPasswordLoging.addTextChangedListener(this);
+        btLogingButton.setOnClickListener(this);
+
+    }
+
+    private void TextChange(CharSequence charSequence, String etToCompare){
+        if ((charSequence.length()>0)&&(etToCompare.length()>0) ) {
+            btLogingButton.setEnabled(true);
+
+        } else {
+            btLogingButton.setEnabled(false);
+
+        }
+        Log.d("button","is enable: "  + btLogingButton.isEnabled());
+    }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                                                            //Metodos de apoyo para TOOLBAR
+    @Override
+    //Creacion de menu
+    public boolean onCreateOptionsMenu(
+            Menu menu) {
+        getMenuInflater().inflate(R.menu.directory, menu);
+        return true;
+    }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    @Override
+    //Adiccion de opcion @param about_us
+    public boolean onOptionsItemSelected(
+            MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.option_menu) {
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if(etUserLoging.getText().hashCode() == charSequence.hashCode()){
+            TextChange(charSequence, etPasswordLoging.getText().toString());
+        }
+        else if(etPasswordLoging.getText().hashCode() == charSequence.hashCode()){
+            TextChange(charSequence, etUserLoging.getText().toString());
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if((Button)view == btLogingButton){
+            Toast.makeText(this,"Se ha iniciado sesión Tio!!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}
+//======================================================================================================================
+/*END TASK*/
---
+++ app/src/main/java/com/example/cesarsantacruz/tw/activity_NewTweet.java
@@ -0,0 +1,81 @@
+package com.example.cesarsantacruz.tw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class activity_NewTweet extends AppCompatActivity {
+    Button btnNewTweet;
+    ProgressBar pbarCharCounter;
+//----------------------------------------------------------------------------------------------------------------------
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setContentView(R.layout.activity__new_tweet);
+
+        btnNewTweet = findViewById(R.id.activity_new_tweet_toolbar_btnNewTweet);
+        EditText etNewTweet = findViewById(R.id.activity_new_tweet_etNewTweet);
+        pbarCharCounter = findViewById(R.id.activity__new_tweet_pbarCharCounter);
+                                                            //Activar Boton de Crear Tweet
+        etNewTweet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                pbarCharCounter.setProgress(editable.length());
+                Log.i("Counter" ,"the counter is in position:"+editable.length());
+                /*CASE*/
+                if (editable.length() > 0) {
+                    btnNewTweet.setEnabled(true);
+                } else {
+                    btnNewTweet.setEnabled(false);
+                }
+
+                if (editable.length() > 100) {
+                    pbarCharCounter.setProgressDrawable
+                            (getDrawable(R.drawable.progress_determinate_almost_full_circular_bar));
+                    if (editable.length()>119){
+                        pbarCharCounter.setProgressDrawable
+                                (getDrawable(R.drawable.progress_determinate_full_circular_bar));
+                    }
+                }
+
+                    else {
+                        pbarCharCounter.setProgressDrawable
+                                (getDrawable(R.drawable.progress_determinate_circular_bar));
+                    }
+                }
+                /*END CASE*/
+
+
+        });
+
+        btnNewTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+//----------------------------------------------------------------------------------------------------------------------
+
+    }
+}
