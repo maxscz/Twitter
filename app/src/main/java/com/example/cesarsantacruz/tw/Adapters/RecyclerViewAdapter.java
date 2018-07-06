2fe1ab7dd922b15f632d79c94790b43acc47a9f2
LeoncioChiunti <chiunty09@gmail.com>
2018-07-06 17:37:33 -0500

    Merge branch 'origin' of https://github.com/maxscz/Twitter
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/cesarsantacruz/tw/Activities/LikesActivity.java
    #	app/src/main/java/com/example/cesarsantacruz/tw/Activities/LoginActivity.java
    #	app/src/main/java/com/example/cesarsantacruz/tw/Activities/MainActivity.java
    #	app/src/main/java/com/example/cesarsantacruz/tw/Adapters/RecyclerViewAdapter.java
    #	app/src/main/java/com/example/cesarsantacruz/tw/Models/TwitterFeed.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/cesarsantacruz/tw/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/cesarsantacruz/tw/Adapters/RecyclerViewAdapter.java
@@ -14,6 +14,7 @@
         import android.widget.RelativeLayout;
         import android.widget.TextView;
 
+        import com.example.cesarsantacruz.tw.Activities.CommentsActivity;
         import com.example.cesarsantacruz.tw.Activities.GalleryActivity;
         import com.example.cesarsantacruz.tw.Activities.LikesActivity;
         import com.example.cesarsantacruz.tw.R;
@@ -69,6 +70,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, LikesActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        holder.imageComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CommentsActivity.class);
                 context.startActivity(intent);
             }
         });
